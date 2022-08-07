1ba17d26f89a5fa938655d37fc596525fe7c5e54
Your Name <you@example.com>
2022-08-07 17:20:10 +0530

    [Megha] Add: Contact Edited

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -7,7 +7,7 @@
            
             Console.WriteLine("------WelCome To Address Book Program------");
             bool end = true;
-            Console.WriteLine("SelectNumber\n1.Add Contact\n2.Display\n3.End Of Program");
+            Console.WriteLine("SelectNumber\n1.Add Contact\n2.Display\n3.EditContact\n4.End Of Program");
             Contact contact = new Contact();
             AddressBookMain addContact = new AddressBookMain();
             while (end)
@@ -23,11 +23,14 @@
                         addContact.Display();
                         break;
                     case 3:
+                        addContact.EditContact();
+                        break;
+                    case 4:
                         end = false;
                         Console.WriteLine("Program Is Ended");
                         break;
                 }
             }
-        }
+        }       
     }
 }
