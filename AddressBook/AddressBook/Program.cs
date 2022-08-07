7e7a8d13ad6fbb3437ed06bc35df7a33f2d063e8
Your Name <you@example.com>
2022-08-07 17:11:29 +0530

    [Megha] Add: Contact Added To Address Book

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -5,6 +5,10 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Address Book");
+            Console.WriteLine("------WelCome To Address Book Program------");
+            AddressBookMain book = new AddressBookMain();
+            book.AddContacts();
+            book.Display();
         }
     }
 }
